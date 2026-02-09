50ad06ab13f386a7bdb3caa100fb0cb77e653d83
nfamula <nfamula@gmail.com>
2026-02-09 18:35:17 -0500

    clean up copy

--- apps/web/app/full-portfolio/page.tsx
+++ apps/web/app/full-portfolio/page.tsx
@@ -5,13 +5,12 @@ export default function FullPortfolioPage() {
   return (
     <main className="p-10 bg-violet-100 min-h-[calc(100vh-9rem)]">
       <h1 className="text-4xl font-bold text-slate-900 mb-8 font-sans">
-        My web projects
+        Web projects
       </h1>
         {/* todo: certifcations, skills, interests, projects */}
         {/* todo: blog: pull in from substack, pull in youtube videos */}
       <p className="text-slate-700 text-lg mb-6">
-        Here are examples of published projects from my professional web development experience, and a demonstration of my coding skills. 
-        I have experience across a variety of industries, including education, finance, and e-commerce, and I am always eager to take on new challenges and learn new technologies. If you are interested in learning more about my work or discussing potential opportunities, please don&apos;t hesitate to <a href="/contact" className="text-violet-700 hover:underline">reach out</a>.
+        Here are examples of published projects from my professional web development experience. I have experience across a variety of industries, including education, finance, and e-commerce, and I am always eager to take on new challenges and learn new technologies. If you are interested in learning more about my work or discussing potential opportunities, please don&apos;t hesitate to <a href="/contact" className="text-violet-700 hover:underline">reach out</a>.
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {cardDetails.map((projectCardData) => (
