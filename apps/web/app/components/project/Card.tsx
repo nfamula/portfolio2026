898d59e01e926d3538ca5ca8bf49f94404a10245
nfamula <nfamula@gmail.com>
2026-02-09 20:39:29 -0500

    add todos

--- apps/web/app/components/project/Card.tsx
+++ apps/web/app/components/project/Card.tsx
@@ -1,8 +1,10 @@
 export default function ProjectCard({ title, content }: { title: string, content: string }) {
   return (
+    // todo: add image and link (if apploicable some are too old)
     <div className="p-6 bg-violet-50 rounded-lg shadow hover:shadow-lg transition">
       <h2 className="text-2xl font-semibold font-sans">{title}</h2>
       <p className="mt-2 text-slate-600 font-serif">{content}</p>
     </div>
+    //todo: add "more" drawer for details
   )
 }
