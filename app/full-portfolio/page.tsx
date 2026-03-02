6ab1b397e5c82a89bd4a5c1bcf12c6c522b437fb
nfamula <nfamula@gmail.com>
2026-03-02 15:59:09 -0500

    add fun animations: home page, contact, projects

--- app/full-portfolio/page.tsx
+++ app/full-portfolio/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import Image from "next/image";
 import ProjectCard from "../components/project/Card";
 import { cardDetails } from "../data/projectCardsData";
 
 export default function FullPortfolioPage() {
   return (
     <main className="p-4 bg-violet-100 min-h-[calc(100vh-9rem)]">
-      <div className="bg-stone-100 border border-gray-200 rounded-lg shadow-[0_4px_10px_rgba(15,23,43,0.05)] p-8 mt-6 mb-10">
+      <div className="bg-stone-100 border border-gray-200 rounded-lg shadow-[0_4px_10px_rgba(15,23,43,0.05)] p-8 mb-10">
         <h1 className="logo-style text-3xl">Web projects</h1>
         {/* todo: certifications, skills, interests, about me */}
         {/* todo: blog: pull in from substack, pull in youtube videos */}
@@ -15,6 +16,7 @@ export default function FullPortfolioPage() {
           technologies. If you are interested in learning more or discussing
           potential opportunities, please don&apos;t hesitate to reach out.
         </p>
+
         <Link
           href="/contact"
           className="bg-slate-700 text-slate-50 px-6 py-3 rounded-md hover:opacity-90 transition"
@@ -35,6 +37,22 @@ export default function FullPortfolioPage() {
           />
         ))}
       </div>
+      <Link href="/">
+        <Image
+          src="/images/Natalie-Famula-floral-logo.png"
+          alt="Natalie Famula floral logo"
+          width={100}
+          height={100}
+          className="object-cover rounded-xl mx-auto mb-10"
+        />
+      </Link>
+      <Image
+        src="/images/Bike.svg"
+        alt="bicycle graphic"
+        width={100}
+        height={100}
+        className="animate-bike-cross object-cover"
+      />
     </main>
   );
 }
